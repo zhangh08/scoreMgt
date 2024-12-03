bf0c00dfc0fb764394a9ef298f33ac42f463da0f
zhangh08 <zhangh2504@gmail.com>
2024-12-03 16:26:43 +0800

    Update string_util.h

--- List/utils/string_util.h
+++ List/utils/string_util.h
@@ -9,6 +9,17 @@
 #include <codecvt>
 using namespace std;
 
+#ifdef _UNICODE
+#define string_t wstring
+#else
+#define string_t string
+#endif
+
+//#define CODE_CONVERT_USE_CPP11
+#define CODE_CONVERT_USE_WIN32
+//#define CODE_CONVERT_USE_ICONV
+
+
 class string_util
 {
 public:
